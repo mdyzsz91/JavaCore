71fe8da02e3fb6d04ff025d00510b4b765a203c8
zhaosong <383927016@qq.com>
2017-07-19 23:40:11 +0800

    void

--- src/java/lang/Void.java
+++ src/java/lang/Void.java
@@ -29,7 +29,8 @@ package java.lang;
  * The {@code Void} class is an uninstantiable placeholder class to hold a
  * reference to the {@code Class} object representing the Java keyword
  * void.
- *
+ *Void类是一个不可实例化的占位符类，它持有一个 代表Java关键字void的Class对象的引用。
+ * final表明这个类是不允许被其他类继承的。
  * @author  unascribed
  * @since   JDK1.1
  */
@@ -39,6 +40,8 @@ class Void {
     /**
      * The {@code Class} object representing 代表 the pseudo-type 伪-类型 corresponding 相应的 to
      * the keyword {@code void}.
+     * 这是一个与关键字void一致的伪类型对象。
+     * 获取void类型的字节码，将其引用赋值给Void类的TYPE属性。表名这是一个void关键字的Class对象。
      */
     @SuppressWarnings("unchecked")
     public static final Class<Void> TYPE = (Class<Void>) Class.getPrimitiveClass("void");//原始的
@@ -46,6 +49,7 @@ class Void {
 
     /*
      * The Void class cannot be instantiated.
+     * 构造函数使用private修饰符，表明这个类不能被实例化。
      */
     private Void() {}
 }
